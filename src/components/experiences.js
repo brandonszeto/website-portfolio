ccefb91d916ee230df30aeec11716e66b03df80c
brandonszeto <brandonszeto@gmail.com>
2023-01-06 23:13:56 -0800

    Playing with css/scss/animations

---
+++ src/components/experiences.js
@@ -0,0 +1,36 @@
+import './styles/experiences.scss';
+
+export default function Experiences() {
+    return(
+<main class="page-content">
+  <div class="card">
+    <div class="content">
+      <h2 class="title">Experience 1</h2>
+      <p class="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
+      <button class="btn">View Trips</button>
+    </div>
+  </div>
+  <div class="card">
+    <div class="content">
+      <h2 class="title">Experience 2</h2>
+      <p class="copy">Plan your next beach trip with these fabulous destinations</p>
+      <button class="btn">View Trips</button>
+    </div>
+  </div>
+  <div class="card">
+    <div class="content">
+      <h2 class="title">Experience 3</h2>
+      <p class="copy">It's the desert you've always dreamed of</p>
+      <button class="btn">Book Now</button>
+    </div>
+  </div>
+  <div class="card">
+    <div class="content">
+      <h2 class="title">Experience 3</h2>
+      <p class="copy">Seriously, straight up, just blast off into outer space today</p>
+      <button class="btn">Book Now</button>
+    </div>
+  </div>
+</main>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.scss';
+import Projects from './components/projects';
+import Experiences from './components/experiences';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div class="EntirePage">
+        <div class="ContentContainer">
+            <h1>Jenny</h1>
+            <Experiences />
+            <Projects />
+        </div>
+   </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
